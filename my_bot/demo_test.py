c1c826d9fadd63a0e6a963d31b308ddc7975b24f
RyougiShiki0 <2044257358@qq.com>
2025-04-16 21:34:25 +0800

    尝试

--- my_bot/demo_test.py
+++ my_bot/demo_test.py
@@ -7,22 +7,38 @@
 @Author  ：RyougiShiki
 @Date    ：2025/4/15 下午5:09 
 """
+
 import os
 
 import botpy
+from botpy.message import Message
 from botpy import logging
+from botpy.message import GroupMessage, Message
 
 from botpy.ext.cog_yaml import read
+
 
 test_config = read(os.path.join(os.path.dirname(__file__), "config.yaml"))
 
 _log = logging.get_logger()
 
 
-class MyBot(botpy.Client):
-    a = 10
-    b = "hello"
+class MyClient(botpy.Client):
+    async def on_ready(self):
+        _log.info(f"robot 「{self.robot.name}」 启动成功!")
+
+    # 回复群聊信息
+    async def on_group_at_message_create(self, message: GroupMessage):
+        messageResult = await message._api.post_group_message(
+            group_openid=message.group_openid,
+            msg_type=0,
+            msg_id=message.id,
+            content=f"这里是初号机（测试版）:{message.content}")
+        _log.info(messageResult)
 
 
 if __name__ == '__main__':
-    print(111)
+
+    intents = botpy.Intents(public_messages=True)
+    client = MyClient(intents=intents)
+    client.run(appid=test_config["appid"], secret=test_config["secret"])
